b09318431ec9fdcebfcabb1b07ca91d62394d8de
laurazp <lzafrap@uoc.edu>
2022-03-10 15:01:33 +0000

    Added new background image in MainActivity

--- app/src/main/java/com/ultimapieza/puzzledroid/MainActivity.java
+++ app/src/main/java/com/ultimapieza/puzzledroid/MainActivity.java
@@ -3,6 +3,9 @@ package com.ultimapieza.puzzledroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.playBtn);
+        playButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // TODO: Modificar acción al hacer click
+                Toast.makeText(getApplicationContext(), "Play button clicked!", Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 }
