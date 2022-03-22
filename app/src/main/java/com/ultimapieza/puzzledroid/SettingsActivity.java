2ae2d4bf762a36432246c5cb6462419dbc4c48c5
SoySkate <101281408+SoySkate@users.noreply.github.com>
2022-03-22 16:02:01 +0100

    Adding settings class and creating xml settings

---
+++ app/src/main/java/com/ultimapieza/puzzledroid/SettingsActivity.java
@@ -0,0 +1,55 @@
+package com.ultimapieza.puzzledroid;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+
+public class SettingsActivity extends AppCompatActivity {
+    SwitchCompat switch1, switch2;
+
+    boolean stateSwitch1, stateSwitch2;
+
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        preferences = getSharedPreferences("PREFS", 0);
+        stateSwitch1 = preferences.getBoolean("switch1", false);
+        stateSwitch2 = preferences.getBoolean("switch2", false);
+
+        switch1 = (SwitchCompat) findViewById(R.id.switch1);
+        switch2 = (SwitchCompat) findViewById(R.id.switch2);
+
+        switch1.setChecked(stateSwitch1);
+        switch2.setChecked(stateSwitch2);
+
+        switch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch1= !stateSwitch1;
+                switch1.setChecked(stateSwitch1);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("switch1", stateSwitch1);
+                editor.apply();
+            }
+        });
+
+        switch2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stateSwitch2= !stateSwitch2;
+                switch2.setChecked(stateSwitch2);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putBoolean("switch2", stateSwitch2);
+                editor.apply();
+            }
+        });
+    }
+}
