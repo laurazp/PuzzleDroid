087e8b2dc1058e8a5d59617caed0606651bc19b9
jpodeschva <92250265+jpodeschva@users.noreply.github.com>
2022-03-23 23:10:54 +0100

    cambios manifest y texto help xml

--- app/src/main/java/com/ultimapieza/puzzledroid/HelpActivity.java
+++ app/src/main/java/com/ultimapieza/puzzledroid/HelpActivity.java
@@ -1,8 +1,19 @@
 package com.ultimapieza.puzzledroid;
 
+import android.os.Bundle;
 import android.widget.Button;
 
-public class HelpActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+    }
 
 
 }
