d4be40f2de37502cbd09f4d386e9e9bb0022f5cc
laurazp <lzafrap@uoc.edu>
2022-04-03 18:38:27 +0100

    Music switch working in Settings (plays/stops music when clicked)

--- app/src/main/java/com/ultimapieza/puzzledroid/SettingsActivity.java
+++ app/src/main/java/com/ultimapieza/puzzledroid/SettingsActivity.java
@@ -2,30 +2,33 @@ package com.ultimapieza.puzzledroid;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Switch;
 import android.widget.Button;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SwitchCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
     Switch switch1, switch2;
-
-
     boolean stateSwitch1, stateSwitch2;
 
-
+    String filePath = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-13.mp3";
+    Intent serviceIntent;
     SharedPreferences preferences;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         Button backButton = findViewById(R.id.backBtn);
 
+        // Lanzamos el servicio para la música
+        serviceIntent = new Intent(this, MyService.class);
+        serviceIntent.putExtra("FilePath", filePath);
 
         preferences = getSharedPreferences("PREFS", 0);
         stateSwitch1 = preferences.getBoolean("switch1", false);
@@ -45,6 +48,35 @@ public class SettingsActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.putBoolean("switch1", stateSwitch1);
                 editor.apply();
+
+                // TODO: PRUEBAS PARA APAGAR Y ENCENDER MÚSICA
+                if (switch1.isChecked()) {
+                    //encender musica
+                    try {
+                        startService(serviceIntent);
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(SettingsActivity.this, "Error starting music.", Toast.LENGTH_SHORT).show();
+                    }
+                    //stateSwitch1 = false;
+                    //SharedPreferences.Editor editor = preferences.edit();
+                    //editor.putBoolean("switch1", false);
+                    //editor.apply();
+                }
+                else {
+                    //apagar musica
+                    try {
+                        stopService(serviceIntent);
+                    }
+                    catch (Exception e) {
+                        Toast.makeText(SettingsActivity.this, "Error stopping music.", Toast.LENGTH_SHORT).show();
+                    }
+
+                    //stateSwitch1 = true;
+                    //SharedPreferences.Editor editor = preferences.edit();
+                    //editor.putBoolean("switch1", true);
+                    //editor.apply();
+                }
             }
         });
 
